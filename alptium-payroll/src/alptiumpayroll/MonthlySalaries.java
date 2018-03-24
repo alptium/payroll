c3722c062f3d3dca663d61dedc170792cf299505
Bojana Lučić <36507048+Bojana06@users.noreply.github.com>
2018-03-24 19:00:55 +0100

    Seaparated Types of Payment

--- alptium-payroll/src/alptiumpayroll/MonthlySalaries.java
+++ alptium-payroll/src/alptiumpayroll/MonthlySalaries.java
@@ -2,8 +2,28 @@ package alptiumpayroll;
 
 public class MonthlySalaries {
 
-	public static void main(String[] args) {
+	private double grossPay;
+	private double taxDeduction;
+	private double netPay;
 
+	public MonthlySalaries(int idNum, double grossPay, double taxDeduction, double netPay) {
+
+		this.grossPay = grossPay;
+		this.taxDeduction = taxDeduction;
+		this.netPay = netPay;
+
+	}
+
+	public double getgrossPay() {
+		return grossPay;
+	}
+
+	public double gettaxDeduction() {
+		return taxDeduction;
+	}
+
+	public double getnetPay() {
+		return netPay;
 	}
 
 }
